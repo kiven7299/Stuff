9b59abd0d7d7b31b8ba6dc7933a8156fd26cac87
Lam Nguyen <60765640+kiven7299@users.noreply.github.com>
2020-04-19 15:19:13 +0700

    Crawl from multiple urls

--- crawl_lms_quiz.py
+++ crawl_lms_quiz.py
@@ -1,42 +1,51 @@
 from bs4 import BeautifulSoup
 import requests as req
-
+import re
 
 def get_beautifulsoup(url, sessionID):
 	resp = req.get(url, cookies={'MoodleSession': sessionID})
 	return BeautifulSoup(resp.content, 'html.parser')
 
 
-def process_beautifulsoup(beautiful_soup_html):
+def make_questions_answers(url_list, sessionID):
 	results = [] # [{"question": "", "choices":[], "right answer": "" }]
-	doc = beautiful_soup_html
+	all_questions = set()
 
-	# get all questions and their correct answer
-	questions = doc.find_all("div", {"class", "qtext"})
-	ans = doc.find_all("div", {"class", "rightanswer"})
+	# get all questions and their correct answer from urls
+	for url in url_list:
 
-	# for each question, get all choices + add to results
-	for i in range(len(questions)):
-		a_question = {} #the dict in 'results' variable
-		temp_list = [] #list to store choices
+		# get beutifulsoup
+		doc = get_beautifulsoup(url, sessionID)
+		questions = doc.find_all("div", {"class", "qtext"})
+		for i in range(len(questions)):
+			question = questions[i].text
+			if question not in all_questions:
 
-		a_question["question"] = questions[i].text
-		if len(ans) == 0:
-			a_question["right answer"] = "No answer."
-		else:
-			a_question["right answer"] = ans[i].text
+				# get question, answers and choices
+				choices = doc.select("#q{} > div.content > div.formulation.clearfix > div.ablock > div.answer > div > label".format(i + 1))
+				ans = doc.select("#q{} > div.content > div.outcome.clearfix > div > div.rightanswer".format(i + 1))
+				
 
-		choices = doc.select("#q{} > div.content > div.formulation.clearfix > div.ablock > div.answer > div > label".format(i + 1))
-		for c in range(len(choices)):
-			temp_list.append(choices[c].text)
-		a_question['choices'] = temp_list
-		results.append(a_question)
+				# process a question
+				a_question = {} #the dict in 'results' variable
+				temp_list = [] #list to store choices
+
+				a_question["question"] = question
+				if len(ans) == 0:
+					a_question["right answer"] = "No answer."
+				else:
+					a_question["right answer"] = ans[0].text
+				
+				for c in range(len(choices)):
+					temp_list.append(choices[c].text)
+				a_question['choices'] = temp_list
+
+				# post process
+				all_questions.add(question) # add the question to all_question set
+				results.append(a_question)
+
 
 	return results
-
-
-def make_questions_answers(url, sessionID):
-	return process_beautifulsoup(get_beautifulsoup(url, sessionID))
 
 
 def add_to_file(questions, filename):
@@ -48,17 +57,25 @@ def add_to_file(questions, filename):
 	f.close()
 
 
+def get_url_list():
+	return re.split(r'\s{0,},\s{0,}', input('Enter url(s) to LMS quizzes [delimeter by \',\']: '))
+
+
 def main():
-	url = input('Enter url to review: ')
+	url_list = get_url_list()
 	sessionID = input('Enter the MoodleSession cookie: ')
 	fileDes = input('Enter filename to save: ')
 
-	try:		
-		questions = make_questions_answers(url, sessionID)
-		add_to_file(questions, fileDes)
-		print("Done!")
-	except Exception as e:
-		print('Error(s) occured: ' + str(e))
+	# try:		
+	# 	questions = make_questions_answers(url_list, sessionID)
+	# 	add_to_file(questions, fileDes)
+	# 	print("Done!")
+	# except Exception as e:
+	# 	print('Error(s) occured: ' + str(e))
+
+	questions = make_questions_answers(url_list, sessionID)
+	add_to_file(questions, fileDes)
+	print("Done!")
 
 
 main()
