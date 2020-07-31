a29f6135144ba6764cb57afcc0331103fe2a0282
Lam Nguyen <60765640+kiven7299@users.noreply.github.com>
2020-07-31 21:37:07 +0700

    add feature: with or without correct answer

--- crawl_lms_quiz.py
+++ crawl_lms_quiz.py
@@ -7,7 +7,25 @@ def get_beautifulsoup(url, sessionID):
 	return BeautifulSoup(resp.content, 'html.parser')
 
 
-def make_questions_answers(url_list, sessionID):
+def get_choices_and_right_answer(bs4_doc, nth, is_ans_available):
+	ans = ''
+	choices = bs4_doc.select("#q{} > div.content > div.formulation.clearfix > div.ablock > div.answer > div > label".format(nth))
+	
+	if is_ans_available:
+		ans = bs4_doc.select("#q{} > div.content > div.outcome.clearfix > div > div.rightanswer".format(nth))
+	else: # if there is no right answer, check if the choice is corrected.
+		ans = 'No Answer.'
+		for choice in bs4_doc.select("#q{} > div.content > div.formulation.clearfix > div.ablock > div.answer > div".format(nth)):
+			if choice.select('input')[0].get('value') == '1': # choosen answer
+				gradetxt = bs4_doc.select('#q{} > div.info > div.grade'.format(nth))[0].text
+				marks = s = re.findall('\d{1,}\.\d{1,}', gradetxt)
+				if marks[0] == marks[1]:
+					ans = choice.select('label')[0].text
+					break
+	return (choices, ans)
+
+
+def make_questions_answers(url_list, sessionID, is_ans_available):
 	results = [] # [{"question": "", "choices":[], "right answer": "" }]
 	all_questions = set()
 
@@ -22,19 +40,16 @@ def make_questions_answers(url_list, sessionID):
 			if question not in all_questions:
 
 				# get question, answers and choices
-				choices = doc.select("#q{} > div.content > div.formulation.clearfix > div.ablock > div.answer > div > label".format(i + 1))
-				ans = doc.select("#q{} > div.content > div.outcome.clearfix > div > div.rightanswer".format(i + 1))
-				
+				# choices = doc.select("#q{} > div.content > div.formulation.clearfix > div.ablock > div.answer > div > label".format(i + 1))
+				# ans = doc.select("#q{} > div.content > div.outcome.clearfix > div > div.rightanswer".format(i + 1))
+				choices, ans = get_choices_and_right_answer(doc, i + 1, is_ans_available)
 
 				# process a question
 				a_question = {} #the dict in 'results' variable
 				temp_list = [] #list to store choices
 
 				a_question["question"] = question
-				if len(ans) == 0:
-					a_question["right answer"] = "No answer."
-				else:
-					a_question["right answer"] = ans[0].text
+				a_question["right answer"] = 'Answer: ' + ans
 				
 				for c in range(len(choices)):
 					temp_list.append(choices[c].text)
@@ -54,6 +69,7 @@ def add_to_file(questions, filename):
 		f.write('{}) {}\n'.format(i + 1, questions[i]['question']))
 		f.write(' {}\n'.format('\n '.join(questions[i]['choices'])))
 		f.write(questions[i]['right answer'] + '\n\n')
+	f.write('\n------------------------------\n')
 	f.close()
 
 
@@ -64,15 +80,18 @@ def get_url_list():
 def main():
 	url_list = get_url_list()
 	sessionID = input('Enter the MoodleSession cookie: ')
-	fileDes = input('Enter filename to save: ')
+	is_ans_available = (input('Answer avaiable? [y|others]: ') == 'y')
+	fileDes = input('Enter filename to save: ')	
 
 	try:		
-		questions = make_questions_answers(url_list, sessionID)
+		questions = make_questions_answers(url_list, sessionID, is_ans_available)
 		add_to_file(questions, fileDes)
 		print("Done!")
 	except Exception as e:
 		print('Error(s) occured: ' + str(e))
 
-
+	# questions = make_questions_answers(url_list, sessionID, is_ans_available)
+	# add_to_file(questions, fileDes)
+	# print("Done!")
 
 main()
