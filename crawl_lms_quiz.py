b73e07e271288e4d5d08064f12d38122cb4e63dc
Lam Nguyen <60765640+kiven7299@users.noreply.github.com>
2020-08-04 13:11:15 +0700

    change data structure and add feature
    + change data structure of results variable to dict
    + still process a question if it is not answered correctly

--- crawl_lms_quiz.py
+++ crawl_lms_quiz.py
@@ -16,35 +16,36 @@ def get_choices_and_right_answer(bs4_doc, nth, is_ans_available):
 	else: # if there is no right answer, check if the choice is corrected.
 		ans = 'No Answer.'
 		for choice in bs4_doc.select("#q{} > div.content > div.formulation.clearfix > div.ablock > div.answer > div".format(nth)):
-			if choice.select('input')[0].get('value') == '1': # choosen answer
+			if choice.select('input')[0].get('checked') == 'checked': # choosen answer
 				gradetxt = bs4_doc.select('#q{} > div.info > div.grade'.format(nth))[0].text
-				marks = s = re.findall('\d{1,}\.\d{1,}', gradetxt)
+				marks = re.findall('\d{1,}\.\d{1,}', gradetxt)
 				if marks[0] == marks[1]:
 					ans = choice.select('label')[0].text
 					break
-	return (choices, ans)
+	return (choices, ans, (ans == 'No Answer.'))
 
 
 def make_questions_answers(url_list, sessionID, is_ans_available):
-	results = [] # [{"question": "", "choices":[], "right answer": "" }]
-	all_questions = set()
+	results = {} # {"question": {"choices":[], "right answer": "", "is_answer_corrected" : True | Flase }}
+
 	# get all questions and their correct answer from urls
 	for url in url_list:
+
 		# get beutifulsoup
 		doc = get_beautifulsoup(url, sessionID)
 		questions = doc.find_all("div", {"class", "qtext"})
 		for i in range(len(questions)):
 			question = questions[i].text
-			if question not in all_questions:
+			if question not in results.keys() or not results[question]['is_answer_corrected']: # this question has been answered correctly
 
 				# get question, answers and choices
-				choices, ans = get_choices_and_right_answer(doc, i + 1, is_ans_available)
-				
+				choices, ans, is_answer_corrected = get_choices_and_right_answer(doc, i + 1, is_ans_available)
+
 				# process a question
 				a_question = {} #the dict in 'results' variable
 				temp_list = [] #list to store choices
 
-				a_question["question"] = question
+				a_question["is_answer_corrected"] = is_answer_corrected
 				a_question["right answer"] = 'Answer: ' + ans
 				
 				for c in range(len(choices)):
@@ -52,17 +53,19 @@ def make_questions_answers(url_list, sessionID, is_ans_available):
 				a_question['choices'] = temp_list
 
 				# post process
-				all_questions.add(question) # add the question to all_question set
-				results.append(a_question)
+				results[question] = a_question
+
+
 	return results
 
 
 def add_to_file(questions, filename):
 	f = open('{}.txt'.format(filename), 'a+', encoding='utf-8')
-	for i in range(len(questions)):
-		f.write('{}) {}\n'.format(i + 1, questions[i]['question']))
-		f.write(' {}\n'.format('\n '.join(questions[i]['choices'])))
-		f.write(questions[i]['right answer'] + '\n\n')
+	tmp_questions = list(questions.keys())
+	for i in range(len(tmp_questions)):
+		f.write('{}) {}\n'.format(i + 1, tmp_questions[i]))
+		f.write(' {}\n'.format('\n '.join(questions[tmp_questions[i]]['choices'])))
+		f.write(questions[tmp_questions[i]]['right answer'] + '\n\n')
 	f.write('\n------------------------------\n')
 	f.close()
 
@@ -83,5 +86,4 @@ def main():
 		print("Done!")
 	except Exception as e:
 		print('Error(s) occured: ' + str(e))
-
 main()
